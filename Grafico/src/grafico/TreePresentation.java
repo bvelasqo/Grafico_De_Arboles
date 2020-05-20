57d55734c8616b41af74721fdc8eb6bc81a46a8b
bvelasqo <bvelasqo@users.noreply.github.com>
2020-05-20 12:04:09 -0500

    version x

--- Grafico/src/grafico/TreePresentation.java
+++ Grafico/src/grafico/TreePresentation.java
@@ -35,24 +35,28 @@ public class TreePresentation extends JPanel {
     public void paint(Graphics grphcs) {
         super.paint(grphcs);
         fontMetrics=grphcs.getFontMetrics();
-        calcularPosicion();
+        CalculatePositions();
+        grphcs.translate(getWidth() / 2, parentToChild);
+         drawTree(grphcs, tree.getRoot(), Integer.MAX_VALUE, Integer.MAX_VALUE, 
+                  fontMetrics.getLeading() + fontMetrics.getAscent());
+         fontMetrics = null;
     }
 
-    private void calcularPosicion() {
+    private void CalculatePositions() {
         postionNodes.clear();
         subtreeSizes.clear();
         BinaryNode root = tree.getRoot();
         if(root!=null){
-            calcularTamañoSubarbol(root);
-            calcularPosicion(root, Integer.MAX_VALUE, Integer.MAX_VALUE, 0);
+            CalculateSizeSubTree(root);
+            CalculatePosition(root, Integer.MAX_VALUE, Integer.MAX_VALUE, 0);
         }
     }
 
-    private Dimension calcularTamañoSubarbol(BinaryNode Currentroot) {
+    private Dimension CalculateSizeSubTree(BinaryNode Currentroot) {
         if(Currentroot.equals(null))
             return new Dimension(0, 0);
-        Dimension leftDimension=calcularTamañoSubarbol(Currentroot.getLeft());
-        Dimension rightDimension=calcularTamañoSubarbol(Currentroot.getRight());
+        Dimension leftDimension=CalculateSizeSubTree(Currentroot.getLeft());
+        Dimension rightDimension=CalculateSizeSubTree(Currentroot.getRight());
         int height=fontMetrics.getHeight()+parentToChild+Math.max(leftDimension.height, rightDimension.height);
         int width=childToChild+leftDimension.width+rightDimension.width;
         Dimension d= new Dimension(height, width);
@@ -60,7 +64,7 @@ public class TreePresentation extends JPanel {
         return d;
     }
 
-    private void calcularPosicion(BinaryNode n, int left, int right, int top) {
+    private void CalculatePosition(BinaryNode n, int left, int right, int top) {
         if(n==null)return;
         Dimension firstDimension=(Dimension) subtreeSizes.get(n.getLeft());
         if(firstDimension==null)
@@ -75,8 +79,19 @@ public class TreePresentation extends JPanel {
           center = left + firstDimension.width + childToChild/2;
       int width = fontMetrics.stringWidth(n.getData()+"");
       postionNodes.put(n,new Rectangle(center - width/2 - 3, top, width + 6, fontMetrics.getHeight()));
-      calcularPosicion(n.getLeft(), Integer.MAX_VALUE, center - childToChild/2, top + fontMetrics.getHeight() + parentToChild);
-      calcularPosicion(n.getRight(), center + childToChild/2, Integer.MAX_VALUE, top + fontMetrics.getHeight() + parentToChild);
+      CalculatePosition(n.getLeft(), Integer.MAX_VALUE, center - childToChild/2, top + fontMetrics.getHeight() + parentToChild);
+      CalculatePosition(n.getRight(), center + childToChild/2, Integer.MAX_VALUE, top + fontMetrics.getHeight() + parentToChild);
+    }
+    
+    private void drawTree(Graphics graphics, BinaryNode n, int x, int y, int sizeFm){
+        if(n==null)return;
+        Rectangle rectangle= (Rectangle) postionNodes.get(n);
+        graphics.drawOval(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        graphics.drawString(n.getData()+"", rectangle.x+3, rectangle.y+sizeFm);
+        if(x!=Integer.MAX_VALUE)
+            graphics.drawLine(x, y, (int)rectangle.x+rectangle.width/2, rectangle.y);
+        drawTree(graphics, n.getLeft(),(int)rectangle.x+rectangle.width/2 , rectangle.y+rectangle.height, sizeFm);
+        drawTree(graphics, n.getRight(),(int)rectangle.x+rectangle.width/2 , rectangle.y+rectangle.height, sizeFm);
     }
     
 }
